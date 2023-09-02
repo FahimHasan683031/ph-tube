2e704700b24651a2cf1b7e473c498fc480c9e64d
FahimHasan683031 <fahimhasan683031@gmail.com>
2023-09-02 13:29:46 +0600

    80% task conplite

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ const mainHandler = async () =>{
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const data = await res.json()
     const categories = data.data
-    console.log(categories)
 
     // categories show
     const tabContainer = document.getElementById('tab-container');
@@ -16,7 +15,6 @@ const mainHandler = async () =>{
         tabContainer.appendChild(tab);
         arrId.push(element.category_id)
     });
-    console.log(arrId)
     
 }
 
@@ -24,70 +22,16 @@ const mainHandler = async () =>{
 // category post show
 let veryId = 'verify'
 let imgBoxId ='imgBox'
-const categoryPost = async(categoryId,isSort= false) => {
+let sorCategoryData;
+const categoryPost = async(categoryId) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const data = await res.json()
     const posts = data.data;
+    sorCategoryData = data.data
     const postContainer = document.getElementById('postContainer');
+
     
-    console.log(posts)
-
-    if(isSort){
-        postContainer.innerHTML = '';
-        document.getElementById('noposts').innerText = '';
-        sortPosts = posts.sort((a,b) => b.others.views.slice(0, -1) - a.others.views.slice(0, -1));
-        sortPosts.forEach(post => {
-            veryId +=2;
-            imgBoxId +=5;
-           const div = document.createElement('div');
-           div.innerHTML = `
-           <div class="  bg-base-100 shadow-xl px-3 pt-5">
-           <figure class="relative" id='${imgBoxId}'><img class='rounded-md w-[312px] h-[180px]' src='${post.thumbnail}' alt="Shoes" /></figure>
-           <div class=" py-7 flex gap-3">
-               <div>
-                   <img class="rounded-full w-12 h-12" src='${post.authors[0].profile_picture}' alt="">
-               </div>
-               <div>
-                   <h2 class=" text-xl font-medium">${post.title}</h2>
-                   <div id='${veryId}'  class="flex gap-3 my-2">
-                       <p class="text-lg font-medium text-gray-500 ">${post.authors[0].profile_name}</p>
-                   </div>
-                   <h3 class="text-lg font-medium text-gray-500"> ${post.others.views} Views</h3>
-                   
-               </div>
-           </div>
-       </div>
-           `
-        postContainer.appendChild(div);
-
-
-        //    verify img set
-        const veContainer = document.getElementById(veryId);
-        if(post.authors[0].verified === true || post.authors[0].verified === ' ' ){
-            const img = document.createElement('img');
-            img.setAttribute('src','images/fi_10629607.jpg')
-            img.classList.add('w-7')
-            veContainer.appendChild(img)
-        }
-
-
-        // time set 
-        const imgBoxTime = document.getElementById(imgBoxId)
-        if(post.others.posted_date){
-            const dateTime = (new String((post.others.posted_date/60).toFixed(2))).split('.');
-            const div = document.createElement('div');
-            div.innerHTML = `
-                <h2 class=" px-2 py-1 rounded-lg bg-slate-900 inline text-white">${dateTime[0]} hrs ${dateTime[1]} min ago</h2>
-            `
-            div.classList.add('text-white', 'absolute', 'bottom-5', 'right-3')
-            imgBoxTime.appendChild(div)
-        }
-        
-           
-        });
-        
-    }else{
-        postContainer.innerHTML = '';
+    postContainer.innerHTML = '';
         document.getElementById('noposts').innerText = '';
         posts.forEach(post => {
             veryId +=2;
@@ -135,11 +79,12 @@ const categoryPost = async(categoryId,isSort= false) => {
             div.classList.add('text-white', 'absolute', 'bottom-5', 'right-3')
             imgBoxTime.appendChild(div)
         }
-        console.log(posts.length)
         
        
            
         });
+
+        // No post massage
         if(posts.length === 0){
             const nodatacontainer = document.getElementById('noposts');
             document.getElementById('noposts').innerText = '';
@@ -151,22 +96,73 @@ const categoryPost = async(categoryId,isSort= false) => {
             div.classList.add('mx-auto', 'text-center', 'mt-36')
             nodatacontainer.appendChild(div)
         }
+
+        
     }
 
-    // console.log(posts)
-   
-    // post show
-    // no post massage
     
-}
+
 mainHandler();
 categoryPost(1000);
 
 
 
-// sort view
+//Post sort view
 function sortView (){
-    categoryPost(1000, true)
+    const sortPosts = sorCategoryData.sort((a,b) => b.others.views.slice(0, -1) - a.others.views.slice(0, -1));
+
+    postContainer.innerHTML = '';
+        document.getElementById('noposts').innerText = '';
+        sortPosts.forEach(post => {
+            veryId +=2;
+            imgBoxId +=5;
+           const div = document.createElement('div');
+           div.innerHTML = `
+           <div class="  bg-base-100 shadow-xl px-3 pt-5">
+           <figure class="relative" id='${imgBoxId}'><img class='rounded-md w-[312px] h-[180px]' src='${post.thumbnail}' alt="Shoes" /></figure>
+           <div class=" py-7 flex gap-3">
+               <div>
+                   <img class="rounded-full w-12 h-12" src='${post.authors[0].profile_picture}' alt="">
+               </div>
+               <div>
+                   <h2 class=" text-xl font-medium">${post.title}</h2>
+                   <div id='${veryId}'  class="flex gap-3 my-2">
+                       <p class="text-lg font-medium text-gray-500 ">${post.authors[0].profile_name}</p>
+                   </div>
+                   <h3 class="text-lg font-medium text-gray-500"> ${post.others.views} Views</h3>
+                   
+               </div>
+           </div>
+       </div>
+           `
+        postContainer.appendChild(div);
+
+
+        //    verify img set
+        const veContainer = document.getElementById(veryId);
+        if(post.authors[0].verified === true || post.authors[0].verified === ' ' ){
+            const img = document.createElement('img');
+            img.setAttribute('src','images/fi_10629607.jpg')
+            img.classList.add('w-7')
+            veContainer.appendChild(img)
+        }
+
+
+        // time set 
+        const imgBoxTime = document.getElementById(imgBoxId)
+        if(post.others.posted_date){
+            const dateTime = (new String((post.others.posted_date/60).toFixed(2))).split('.');
+            const div = document.createElement('div');
+            div.innerHTML = `
+                <h2 class=" px-2 py-1 rounded-lg bg-slate-900 inline text-white">${dateTime[0]} hrs ${dateTime[1]} min ago</h2>
+            `
+            div.classList.add('text-white', 'absolute', 'bottom-5', 'right-3')
+            imgBoxTime.appendChild(div)
+        }
+        
+       
+           
+        });
 }
 
 
